4dcec2c1b72ea505def525f56dd1d1f5a6af13a2
larrygf02 <raulgf_02@hotmail.com>
2020-04-02 22:52:38 -0500

    Go mysql read users

--- main.go
+++ main.go
@@ -7,6 +7,10 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+type User struct {
+	Name string `json: "name"`
+}
+
 func main() {
 	fmt.Println("Go mysql Tutorial")
 
@@ -18,11 +22,23 @@ func main() {
 	defer db.Close()
 	fmt.Println("Succesfully connected to mysql")
 
-	insert, err := db.Query("Insert into users(name) values ('Josue')")
+	/* insert, err := db.Query("Insert into users(name) values ('Josue')")
 	if err != nil {
 		panic(err.Error())
 	}
 	defer insert.Close()
-
 	fmt.Println("Succesfully insert")
+	*/
+	results, err := db.Query("SELECT name FROM users")
+	if err != nil {
+		panic(err.Error())
+	}
+	for results.Next() {
+		var user User
+		err = results.Scan(&user.Name)
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Println(user.Name)
+	}
 }
